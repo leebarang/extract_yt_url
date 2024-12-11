3f1efba3a09c816277791bfa786a741fedc14cd8
이바랑 <nb040705@gmail.com>
2024-12-12 08:14:31 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="extract_yt_url", # Replace with your own username
+    version="0.0.1",
+    author="leebarang", # author_email="xxx@xxx.com",
+    description="A training package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leebarang/extract_yt_url",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.12.1",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Windows 11 64bit",
+    ],
+    python_requires='>=3.6',
+)
