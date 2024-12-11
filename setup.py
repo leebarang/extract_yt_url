c2bd321c86d38660275a64873628092467d1a131
이바랑 <nb040705@gmail.com>
2024-12-12 08:53:01 +0900

    bug fix

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        "Programming Language :: Python :: 3.12.1",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
     ],
     python_requires='>=3.6',
